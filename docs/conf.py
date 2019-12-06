06d9195ca78b2fadb22205ffca7e74a6cc0bb23d
Adin Hodovic <hodovicadin@gmail.com>
2019-12-06 02:57:12 +0100

    Add documentation

---
+++ docs/conf.py
@@ -0,0 +1,19 @@
+extensions = ["sphinx.ext.intersphinx"]
+
+master_doc = "index"
+
+project = "django-google-optimize"
+copyright = "Adin Hodovic and individual contributors."
+
+# The short X.Y version.
+version = "0.1"
+# The full version, including alpha/beta/rc tags.
+release = "0.1.0"
+
+intersphinx_mapping = {
+    "django": (
+        "https://docs.djangoproject.com/en/stable",
+        "https://docs.djangoproject.com/en/stable/_objects/",
+    ),
+    "python": ("https://docs.python.org/3", None),
+}
