19fbd95ed5a8da2450634e9d36632796950760d9
Adin Hodovic <hodovicadin@gmail.com>
2019-12-23 17:57:26 +0100

    Log when settings are not defined

--- google_optimize/context_processors.py
+++ google_optimize/context_processors.py
@@ -4,6 +4,8 @@ from .utils import get_experiments_variants
 
 
 def google_experiment(request):
-    experiments = settings.GOOGLE_OPTIMIZE_EXPERIMENTS
+
+    experiments = getattr(settings, "GOOGLE_OPTIMIZE_EXPERIMENTS", None)
+
     experiments_variants = get_experiments_variants(request, experiments)
     return {"google_optimize": experiments_variants}
