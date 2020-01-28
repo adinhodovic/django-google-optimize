88009d0fc646bade7ecc97732bd31c245aa25371
Adin Hodovic <hodovicadin@gmail.com>
2020-01-28 23:49:24 +0100

    Weh

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -17,7 +17,7 @@ def test_request_middleware(client):
 @pytest.mark.django_db
 def test_template_middleware(client):
     exp = GoogleExperimentFactory(experiment_alias="redesign")
-    variant = ExperimentVariantFactory(index=1, alias="new_design", experiment=exp)
+    ExperimentVariantFactory(index=1, alias="new_design", experiment=exp)
     client.cookies["_gaexp"] = f"GAX1.2.{exp.experiment_id}.18147.1"
     response = client.get("/test")
     assert response.content == b"new_design\n"
