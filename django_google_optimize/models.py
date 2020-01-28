858574df0132becd4b0016bb1e813e8ad551dd19
Adin Hodovic <hodovicadin@gmail.com>
2020-01-28 23:38:23 +0100

    Update migrations

--- django_google_optimize/models.py
+++ django_google_optimize/models.py
@@ -4,14 +4,14 @@ from django.db import models
 class GoogleExperiment(models.Model):
     experiment_id = models.CharField(max_length=50, unique=True)
     experiment_alias = models.CharField(
-        max_length=30, unique=True, blank=True, null=True, required=False
+        max_length=30, unique=True, blank=True, null=True
     )
     active = models.BooleanField()
 
 
 class ExperimentVariant(models.Model):
     index = models.IntegerField()
-    alias = models.CharField(max_length=50, blank=False, null=True, required=False)
+    alias = models.CharField(max_length=50, blank=True, null=True)
     experiment = models.ForeignKey(
         GoogleExperiment,
         null=True,
