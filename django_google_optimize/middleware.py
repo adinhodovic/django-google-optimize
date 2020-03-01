3cd72593e6e5fb1e7c83d6da1449feea66887a80
Dani Hodovic <danihodovic@users.noreply.github.com>
2020-03-01 16:44:09 +0100

    Add context manager to force a specific variant in tests (#34)

--- django_google_optimize/middleware.py
+++ django_google_optimize/middleware.py
@@ -1,3 +1,5 @@
+from contextlib import ContextDecorator
+
 from .utils import get_experiments_variants
 
 
@@ -7,3 +9,17 @@ def google_optimize(get_response):
         return get_response(request)
 
     return middleware
+
+
+class override_google_optimize(ContextDecorator):
+    def __init__(self, experiments=None):
+        self.experiments = experiments
+        self.original_fn = get_experiments_variants
+
+    def __enter__(self):
+        global get_experiments_variants  # pylint: disable=global-statement
+        get_experiments_variants = lambda _: self.experiments
+
+    def __exit__(self, exc_type, exc, exc_tb):
+        global get_experiments_variants  # pylint: disable=global-statement
+        get_experiments_variants = self.original_fn
