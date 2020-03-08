00b954f471a7e2f615af6055a2b651cb42ba576b
Adin Hodovic <hodovicadin@gmail.com>
2020-03-08 21:09:34 +0100

    Return always an empty object (#39)

--- django_google_optimize/middleware.py
+++ django_google_optimize/middleware.py
@@ -5,7 +5,7 @@ from .utils import get_experiments_variants
 
 def google_optimize(get_response):
     def middleware(request):
-        request.google_optimize = get_experiments_variants(request)
+        request.google_optimize = get_experiments_variants(request) or {}
         return get_response(request)
 
     return middleware
